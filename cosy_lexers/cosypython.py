8582c6f3f5a9deb01db1d184e8adce4177432201
Jekannadar <constantin.chaumet@tu-dortmund.de>
2025-06-18 17:28:58 +0200

    Initial commit

---
+++ cosy_lexers/cosypython.py
@@ -0,0 +1,43 @@
+from functools import partial
+
+from pygments.lexers.python import PythonLexer
+from pygments.token import Keyword, Name
+
+
+__all__ = ("CosyPythonLexer",)
+
+
+class CosyPythonLexer(PythonLexer):
+    name = "CosyPythonLexer"
+    aliases = ["cosy-py"]
+    mimetypes = []
+    filenames = []
+
+    EXTRA_PSEUDO_KEYWORDS = {
+        "parameter",
+        "argument",
+        "constraint",
+        "parameter_constraint",
+        "suffix",
+        "DSL",
+        "Constructor",
+        "CoSy",
+        "Arrow",
+        "Intersection",
+        "Tree",
+        "Inspector",
+        "Container",
+        "SolutionSpace",
+        "Taxonomy",
+        "Subtypes",
+        "Tree",
+        "Literal"
+    }
+
+    def get_tokens_unprocessed(self, text):
+        for index, token, value in \
+                PythonLexer.get_tokens_unprocessed(self, text):
+            if token is Name and value in self.EXTRA_PSEUDO_KEYWORDS:
+                yield index, Keyword.Pseudo, value
+            else:
+                yield index, token, value
